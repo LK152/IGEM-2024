b4c043921e6f6440f53d112eb0fa886f4bd9fb17
Terrence Hsueh <terrencehsueh@TerrencedeMacBook-Air.local>
2024-07-11 11:02:18 +0800

    added homepage / moving boat

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import Team from './pages/Team';
+import Home from './pages/Home';
 
 const App = () => {
 	return (
 		<Routes>
+			<Route path='/' element={<Home />} />
 			<Route path='/team' element={<Team />} />
 		</Routes>
 	);
